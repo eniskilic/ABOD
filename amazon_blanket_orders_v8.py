96f586a346e0512a7228016ced42b57c921c289f
eniskilic <eniskilic@gmail.com>
2025-10-20 20:22:57 -0400

    Update print statement from 'Hello' to 'Goodbye'

--- amazon_blanket_orders_v8.py
+++ amazon_blanket_orders_v8.py
@@ -129,7 +129,7 @@ def upload_to_airtable(dataframe):
                         "records": [{
                             "fields": {
                                 "Buyer Name": order_row['Buyer Name'],
-                                "Order ID": [airtable_order_id],
+                                "Order ID Link": [airtable_order_id],
                                 "Customization Name Placement": item_row['Customization Name'],
                                 "Quantity": int(item_row['Quantity']),
                                 "Blanket Color": item_row['Blanket Color'],
